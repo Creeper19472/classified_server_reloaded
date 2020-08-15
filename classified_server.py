5169da8e9f68e2109ea425761c8f42e97645c01c
Creeper19472 <creeper19472@crpteam.club>
2020-08-15 19:45:39 +0800

    Update

--- classified_server.py
+++ classified_server.py
@@ -1,6 +1,6 @@
 ﻿# -*- coding: UTF-8 -*-
 
-VERSION = "0.3.0a1"
+VERSION = "0.3.0a2"
 
 import sys, os, json, socket, sqlite3, rsa, configparser, gettext, time, random, threading, string
 
@@ -63,23 +63,38 @@ if os.path.exists('_classified_initialized') == False:
     except KeyError:
         print('Value is invaild.')
         sys.exit()
-    config = configparser.ConfigParser()
-    config.read('./config/config.ini')
-    config.set('SERVER', 'LANGUAGE', lang)
-    config.write(open('./config/config.ini', 'w'))
 
     dbconn = sqlite3.connect('./cfs-content/database/sqlite3.db')
     dbcursor = dbconn.cursor()
-    with open('./cfs-content/database/setup', 'r', encoding='utf-8') as scriptfile:
-        script = scriptfile.read()
-        dbcursor.executescript(script)
+    '''with open('./cfs-content/database/setup', 'r', encoding='utf-8') as scriptfile:
+        for line in scriptfile:
+            dbcursor.execute(line)
+        print(dbconn.total_changes)
+        dbconn.commit()
+        dbconn.close()''' # CFS-2020081501 : Can't run scripts from files.
+    dbcursor.executescript('''
+        create table auth(username, password, authlevel);
+        insert into auth values('master', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 5);
+        create table server(key, value);
+        insert into server values('host', '0.0.0.0');
+        insert into server values('port', '5104');
+        insert into server values('language', '%s');
+        ''' % lang)
+    print('Total Changes: %s.' % dbconn.total_changes)
+    dbconn.commit()
+    dbconn.close()
     with open("_classified_initialized", "w") as x:
         x.write('\n')
 
-config = configparser.ConfigParser()
-config.read('./config/config.ini')
+### INIT SQLITE3 ###
+dbconn = sqlite3.connect('./cfs-content/database/sqlite3.db')
+dbcursor = dbconn.cursor()
+settings = dict(dbcursor.execute('select key, value from server'))
+dbconn.close()
+### END SQLITE3 ###
 
-lang = config.get("SERVER", "LANGUAGE")
+lang = settings['language']
+
 es = gettext.translation(
         'cfs_server',
         localedir = 'locale',
@@ -88,10 +103,9 @@ es = gettext.translation(
         )
 es.install()
 
-svcinfo = ()
+svcinfo = (settings['host'], int(settings['port']))
 
 sqlite3.connect('./cfs-content/database/sqlite3.db')
-
 
 server.bind(svcinfo)
 server.listen(15)
@@ -101,18 +115,15 @@ print(StrFormat.INFO() + _("Verifying plugin information ..."))
 time2 = time.time() - time1
 print(StrFormat.INFO() + _("Done(%ss)!") % time2)
 
-with open("./secure/e.pem", "rb") as x:
+with open("./cfs-content/cert/e.pem", "rb") as x:
     ekey = x.read()
-with open("./secure/f.pem", "rb") as x:
+with open("./cfs-content/cert/f.pem", "rb") as x:
     fkey = x.read()
 
 # salt = ''.join(random.sample(string.ascii_letters + string.digits, 8))
 # print(letscrypt.BLOWFISH.Encrypt('aaaaaaa', salt))
 
 while True:
-    if EnablePlugins == True:
-        for i in lists:
-            exec(i + '.main()')
     conn, addr = server.accept() # 等待链接,多个链接的时候就会出现问题,其实返回了两个值
     ThreadNewName = "Thread-%s" % random.randint(1,10000)
     NewThread = MainThread(1, ThreadNewName, 1)
---
+++ database.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+dbconn = sqlite3.connect('./cfs-content/database/sqlite3.db')
+dbcursor = dbconn.cursor()
+### END SQLITE3 ###
+
+settings = dict(dbcursor.execute('select key, value from server'))
+print(settings)
+for row in settings:
+    print('Key = %s' % row[0])
+    print('Value = %s' % row[1])
